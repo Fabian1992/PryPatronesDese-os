64dcd31f8a02cf16f9841817e600be98be3d830e
FabianLopez <fabi.lopez1992@gmail.com>
2016-10-29 22:58:27 -0500

    primera

---
+++ src/Clases/Dao.java
@@ -0,0 +1,16 @@
+package Clases;
+
+
+
+public class Dao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double d=Math.random();
+			staticContador.contador1();
+			new staticContador();
+			new staticContador();
+			System.out.println("Contador "+d);
+	}		
+
+}
---
+++ src/Clases/staticContador.java
@@ -0,0 +1,14 @@
+package Clases;
+
+public class staticContador {
+	public static int contador = 0;
+
+	public staticContador() {
+		contador++;
+	}
+
+	public static int contador1() {
+		return contador;
+
+	}
+}
